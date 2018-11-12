9b3aedf2d2c4bd0463649d0c8d882ccfec7463d3
legends <legends.zhang@qiffa.com>
2018-11-12 20:07:25 +0800

    first time init

---
+++ init.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+PROJ_BASE=`pwd`
+
+ln -f -s ${PROJ_BASE}/zsh/.zshrc ~/.zshrc
+ln -F -s ${PROJ_BASE}/zsh/.oh-my-zsh ~/.oh-my-zsh
+
+ln -f -s ${PROJ_BASE}/screen/.screenrc ~/.screenrc
+
+ln -f -s ${PROJ_BASE}/tmux/.tmux.conf ~/.tmux.conf
+
+ln -f -s ${PROJ_BASE}/vim/.vimrc ~/.vimrc
+
